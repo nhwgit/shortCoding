63dfda163fd76a9cc37b46bfb9a6b54b92955f6b
Hyeonwoo No <141965854+nhwgit@users.noreply.github.com>
2026-04-20 05:13:31 +0900

    [1006: 버전 29]

--- 1006.rb
+++ 1006.rb
@@ -1,5 +1,4 @@
-n=0
-w=0
+n,w=0
 F=->a,b,c,d,e{a[c]+b[d]>e ?2:1}
 H=->l,s{l[0],l[-1]=l[-1],l[0];x=[0,1,*0*n];y=x.dup;z=[0,F[l,s,0,0,w],*0*n]
 (2...n).map{|i|z[i]=[1+y[i]=[r=z[j=i-1]+1,x[j]+q=F[s,s,k=i-2,j,w]].min,1+x[i]=[r,y[j]+p=F[l,l,k,j,w]].min,r-1+F[l,s,j,j,w],z[k]+p+q].min}
