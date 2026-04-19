c0e33cd851ef213051329ada655cee403b199b2d
Hyeonwoo No <141965854+nhwgit@users.noreply.github.com>
2026-04-20 05:21:21 +0900

    [1006: 버전 61]

--- 1006.rb
+++ 1006.rb
@@ -1,5 +1,5 @@
 R=->{gets.split.map &:to_i}
 R[][0].times{n,w=R[];l=R[]<<w;s=R[]<<w
-p (0..3).map{l,s=s,l;l[0],l[n]=l[n],l[j=0];x=[1];y=[1];z=0,2;F=->a{w<a[j-1]+a[j]?2:1}
-n.times{|i|z<<[1+y[j+=1]=[r=z[j]+1,x[i]+q=F[s]].min,1+x[j]=[r,y[i]+p=F[l]].min,w<l[j]+s[j]?r+1:r,z[i]+p+q].min}
+p (0..3).map{l,s=s,l;l[0],l[n]=l[n],l[j=0];x=[1];y=[1];z=0,2
+n.times{|i|F=->a{w<a[i]+a[j]?2:1};z<<[1+y[j+=1]=[r=z[j]+1,x[i]+q=F[s]].min,1+x[j]=[r,y[i]+p=F[l]].min,w<l[j]+s[j]?r+1:r,z[i]+p+q].min}
 z[-1]}.min-2}
