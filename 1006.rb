ce6409e870dd78af34c51bb5580eb35d647dd706
Hyeonwoo No <141965854+nhwgit@users.noreply.github.com>
2026-04-20 05:13:03 +0900

    [1006: 버전 27]

--- 1006.rb
+++ 1006.rb
@@ -1,10 +1,10 @@
 n=0
 w=0
 F=->a,b,c,d,e{a[c]+b[d]>e ?2:1}
-G=->p{p[0],p[-1]=p[-1],p[0];p}
-H=->l,s{x=[0,1,*0*n];y=x.dup;z=[0,F[l,s,0,0,w],*0*n]
+G=->p{p[0],p[-1]=p[-1],p[0]}
+H=->l,s{G[l];x=[0,1,*0*n];y=x.dup;z=[0,F[l,s,0,0,w],*0*n]
 (2...n).map{|i|z[i]=[1+y[i]=[r=z[j=i-1]+1,x[j]+q=F[s,s,k=i-2,j,w]].min,1+x[i]=[r,y[j]+p=F[l,l,k,j,w]].min,r-1+F[l,s,j,j,w],z[k]+p+q].min}
 z[-1]}
 R=->{gets.split.map &:to_i}
 gets.to_i.times{n,w=R[];n+=2
-p [H[l=R[]<<w,s=R[]<<w],H[l,G[s]],H[G[l],s],H[l,G[s]]].min-2}
+p [H[l=R[]<<w,s=R[]<<w],H[l,s],H[s,l],H[l,s]].min-2}
