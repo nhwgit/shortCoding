4fe839a651fa55b8b2ff189ae710f5a841c3c239
Hyeonwoo No <141965854+nhwgit@users.noreply.github.com>
2026-04-20 05:08:30 +0900

    [1006: 버전 12]

--- 1006.rb
+++ 1006.rb
@@ -4,9 +4,10 @@ H=->n,m,l,s{x=[0]*n;y=x.dup;z=x.dup
 x[1]=y[1]=1;z[1]=F[l,s,0,0,m]
 (2...n).map{|i|j=i-1;k=i-2
 p=F[l,l,k,j,m]
+q=F[s,s,k,j,m]
 x[i]=[z[j]+1,y[j]+p].min
-y[i]=[z[j]+1,x[j]+F[s,s,k,j,m]].min
-z[i]=[y[i]+1,x[i]+1,z[j]+F[l,s,j,j,m],z[k]+p+F[s,s,k,j,m]].min}
+y[i]=[z[j]+1,x[j]+q].min
+z[i]=[y[i]+1,x[i]+1,z[j]+F[l,s,j,j,m],z[k]+p+q].min}
 z[-1]}
 R=->{gets.split.map &:to_i}
 gets.to_i.times{n,w=R[];l=R[]<<w
