c083babf6896149e618ac0efa23a85888d92313d
Hyeonwoo No <141965854+nhwgit@users.noreply.github.com>
2026-04-20 05:04:58 +0900

    [1006: 버전 3]

--- 1006.rb
+++ 1006.rb
@@ -1,18 +1,10 @@
-c=lambda{|a,b,x,y,m|(a[x]+b[y]>m ?1:0)+1}
-w=lambda{|a|a[0],a[-1]=a[-1],a[0]}
-d=lambda{|r,m,l,s|o=[0]*r;i=[0]*r;y=[0]*r;o[1]=1;i[1]=1;y[1]=c.call(l,s,0,0,m);(2...r).each{|x|j=x-1;k=x-2;o[x]=[y[j]+1,i[j]+c.call(l,l,k,j,m)].min;i[x]=[y[j]+1,o[j]+c.call(s,s,k,j,m)].min;y[x]=[i[x]+1,o[x]+1,y[j]+c.call(l,s,j,j,m),y[k]+c.call(l,l,k,j,m)+c.call(s,s,k,j,m)].min};y[r-1]}
-t=gets.to_i
-t.times do
-n,m=gets.split.map(&:to_i)
-r=n+2
-l=gets.split.map(&:to_i)<<m
-s=gets.split.map(&:to_i)<<m
-ans=[d.call(r,m,l,s)]
-w.call(s)
-ans<<d.call(r,m,l,s)
-w.call(l)
-ans<<d.call(r,m,l,s)
-w.call(s)
-ans<<d.call(r,m,l,s)
-puts ans.min-2
-end
+f=->p,q,r,c,m{p[r]+q[c]>m ?2:1}
+g=->p{p[0],p[-1]=p[-1],p[0]}
+h=->n,m,l,s{x=[0]*n;y=[0]*n;z=[0]*n;x[1]=y[1]=1;z[1]=f[l,s,0,0,m];(2...n).each{|i|j=i-1;k=i-2;x[i]=[z[j]+1,y[j]+f[l,l,k,j,m]].min;y[i]=[z[j]+1,x[j]+f[s,s,k,j,m]].min;z[i]=[y[i]+1,x[i]+1,z[j]+f[l,s,j,j,m],z[k]+f[l,l,k,j,m]+f[s,s,k,j,m]].min};z[n-1]}
+gets.to_i.times{
+A,B=gets.split.map &:to_i
+C=A+2
+L=(gets.split.map &:to_i)<<B
+S=(gets.split.map &:to_i)<<B
+p [h[C,B,L,S],(g[S];h[C,B,L,S]),(g[L];h[C,B,L,S]),(g[S];h[C,B,L,S])].min-2
+}
