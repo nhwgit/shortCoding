c093979abd80691d84db366af621c1344aed7102
Hyeonwoo No <141965854+nhwgit@users.noreply.github.com>
2026-04-26 16:39:04 +0900

    버전 9

--- 10350.rb
+++ 10350.rb
@@ -1,1 +1,1 @@
-n,*a=$<.read.split.map &:to_i;s=a.sum;p (0...n).sum{|i|k=0;a.sum{(k+=a[(i+=1)%n])<0?-(k/s):0}}
+n,*a=$<.read.split.map &:to_i;s=a.sum;p n.times.sum{k=0;a.rotate!.sum{(k+=_1)<0?-(k/s):0}}
